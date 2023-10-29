4679ca607759144207f6b2c9a0f8a88a483d4143
edward <edward@pop-os.localdomain>
2023-10-29 11:54:28 +0200

    Migrate to spring 3 started

--- src/main/java/telran/java48/security/AuthorizationConfiguration.java
+++ src/main/java/telran/java48/security/AuthorizationConfiguration.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 
 @Configuration
+@EnableWebSecurity
 public class AuthorizationConfiguration {
 
 	@Bean
@@ -19,23 +22,23 @@ public class AuthorizationConfiguration {
         http.csrf(csrf -> csrf.disable());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.addFilterAfter(new ExpiredPasswordFilter(), BasicAuthenticationFilter.class);
-		http.authorizeRequests(authorize -> authorize
-				.mvcMatchers("/account/register", "/forum/posts/**")
+		http.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers("/account/register", "/forum/posts/**")
 					.permitAll()
-				.mvcMatchers("/account/user/{login}/role/{role}")
+				.requestMatchers("/account/user/{login}/role/{role}")
 					.hasRole("ADMINISTRATOR")
-				.mvcMatchers(HttpMethod.PUT, "/account/user/{login}")
-					.access("#login == authentication.name")
-				.mvcMatchers(HttpMethod.DELETE, "/account/user/{login}")
-					.access("#login == authentication.name or hasRole('ADMINISTRATOR')")
-				.mvcMatchers(HttpMethod.POST, "/forum/post/{author}")
-        			.access("#author == authentication.name")
-        		.mvcMatchers(HttpMethod.PUT, "/forum/post/{id}/comment/{author}")
-        			.access("#author == authentication.name")
-        		.mvcMatchers(HttpMethod.PUT, "/forum/post/{id}")
-        			.access("@customSecurity.checkPostAuthor(#id, authentication.name)")
-        		.mvcMatchers(HttpMethod.DELETE, "/forum/post/{id}")
-        			.access("@customSecurity.checkPostAuthor(#id, authentication.name) or hasRole('MODERATOR')")
+				.requestMatchers(HttpMethod.PUT, "/account/user/{login}")
+					.access(new WebExpressionAuthorizationManager("#login == authentication.name"))
+				.requestMatchers(HttpMethod.DELETE, "/account/user/{login}")
+					.access(new WebExpressionAuthorizationManager("#login == authentication.name or hasRole('ADMINISTRATOR')"))
+				.requestMatchers(HttpMethod.POST, "/forum/post/{author}")
+        			.access(new WebExpressionAuthorizationManager("#author == authentication.name"))
+        		.requestMatchers(HttpMethod.PUT, "/forum/post/{id}/comment/{author}")
+        			.access(new WebExpressionAuthorizationManager("#author == authentication.name"))
+        		.requestMatchers(HttpMethod.PUT, "/forum/post/{id}")
+        			.access(new WebExpressionAuthorizationManager("@customSecurity.checkPostAuthor(#id, authentication.name)"))
+        		.requestMatchers(HttpMethod.DELETE, "/forum/post/{id}")
+        			.access(new WebExpressionAuthorizationManager("@customSecurity.checkPostAuthor(#id, authentication.name) or hasRole('MODERATOR')"))
 				.anyRequest()
 					.authenticated()
 		);
--- src/main/java/telran/java48/security/ExpiredPasswordFilter.java
+++ src/main/java/telran/java48/security/ExpiredPasswordFilter.java
@@ -2,17 +2,17 @@ package telran.java48.security;
 
 import java.io.IOException;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.http.HttpMethod;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class ExpiredPasswordFilter extends GenericFilterBean {
 
